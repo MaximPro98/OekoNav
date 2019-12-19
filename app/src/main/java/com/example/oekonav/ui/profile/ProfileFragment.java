ce7d4f3f60f0f1867fdd6460d7d6a2f40dc1a3a8
hatechs <hassan@ha-techs.de>
2019-12-19 18:29:28 +0100

    Profile Image update

--- app/src/main/java/com/example/oekonav/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/oekonav/ui/profile/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.oekonav.ui.profile;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,17 +16,36 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.oekonav.R;
+import com.esafirm.imagepicker.features.ImagePicker;
+import com.esafirm.imagepicker.features.ReturnMode;
+import com.esafirm.imagepicker.model.Image;
+
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
-
+    ImageView userImage;
     private ProfileViewModel profileViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
+        userImage = root.findViewById(R.id.img_profile_view);
+
+        userImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.create(ProfileFragment.this)
+                        .returnMode(ReturnMode.ALL) // set whether pick action or camera action should return immediate result or not. Only works in single mode for image picker
+                        .folderMode(true) // set folder mode (false by default)
+                        .single()
+                        .toolbarFolderTitle("Folder") // folder selection title
+                        .toolbarImageTitle("Tap to select")
+                        .toolbarDoneButtonText("DONE") // done button text
+                        .start(0);
+            }
+        });
         profileViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -33,4 +55,13 @@ public class ProfileFragment extends Fragment {
 
         return root;
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        List<Image> images = ImagePicker.getImages(data);
+        if (images != null && !images.isEmpty()) {
+            userImage.setImageBitmap(BitmapFactory.decodeFile(images.get(0).getPath()));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
